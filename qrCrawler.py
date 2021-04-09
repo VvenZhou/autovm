0aa46d3acd1e94ffe8657743a08ec37cf1600e92
Vvenzhou <1418221763@qq.com>
2021-04-09 09:58:35 +0800

    Ver alpha 0.1: change vmessOut datatype to queue

--- qrCrawler.py
+++ qrCrawler.py
@@ -16,7 +16,7 @@ vpingName = "./vping_c3_o5"
 threadingNum = 200
 threads = []
 
-# vmessQueue = queue.Queue()
+vmessQueue = queue.Queue()
 
 class vmessPingThread(threading.Thread):
     def __init__(self, vmessPart):
@@ -31,7 +31,7 @@ class vmessPingThread(threading.Thread):
             if runPing.stdout.split('\n')[-2] != 'rtt min/avg/max = 0/0/0 ms' and len(runPing.stdout.split('\n')) > 5:
                 vmessGood.append(_vm)
             for _vm in vmessGood:
-                print(_vm)
+                vmessQueue.put(_vm)
 
 with open(fileNameRead, 'r') as f:
     data = f.read()
@@ -41,7 +41,7 @@ with open(fileNameRead, 'r') as f:
     vmess = []
     for _tr in trs:
         while(threading.activeCount() >= threadingNum):
-            time.sleep(5)
+            time.sleep(3)
         vmess.append(_tr.xpath('td/a')[0].attrib['data-raw'] + '\n')
         thread = vmessPingThread(vmess)
         threads.append(thread)
@@ -51,4 +51,9 @@ with open(fileNameRead, 'r') as f:
 for index, thread in enumerate(threads):
     print('waitting for: ', index)
     thread.join()
+
 print("finished")
+
+while vmessQueue.empty() is not True:
+    print(vmessQueue.get())
+
